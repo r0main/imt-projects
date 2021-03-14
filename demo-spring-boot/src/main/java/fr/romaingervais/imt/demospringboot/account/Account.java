caa79dd91e7ee7c24b7c2b9b387d1ac4cf352804
Romain GERVAIS <romain.ger@gmail.com>
2021-03-14 22:26:20 +0100

    Correction pour faire fonctionner save/update

--- demo-spring-boot/src/main/java/fr/romaingervais/imt/demospringboot/account/Account.java
+++ demo-spring-boot/src/main/java/fr/romaingervais/imt/demospringboot/account/Account.java
@@ -1,14 +1,34 @@
 package fr.romaingervais.imt.demospringboot.account;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("accounts")
-public class Account {
+public class Account implements Persistable<String> {
+
     @Id
     private String accountId;
 
     private double total;
+
+    @Transient
+    boolean isNew = false;
+
+    @Override
+    public String getId() {
+        return accountId;
+    }
+
+    public void setNew(boolean isNew) {
+        this.isNew = isNew;
+    }
+
+    @Override
+    public boolean isNew() {
+        return isNew;
+    }
 
     public String getAccountId() {
         return accountId;
